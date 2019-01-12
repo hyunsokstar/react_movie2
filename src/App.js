0b969b5c25c431d722d74e67de657edd1d8d6502
hyun2 <dktmvktm@daum.net>
2019-01-12 10:42:04 +0900

    recent_update
    recent_update

--- src/App.js
+++ src/App.js
@@ -10,44 +10,31 @@ class App extends Component {
 
 	}
 
-	componentWillMount() {
-		console.log("component Will Mount");
+	_callApi = () => {
+		return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+			.then(potato => potato.json())
+			.then(json => json.data.movies)
+			.catch(err => console.log(err))
 	}
 
-	componentDidMount() {
-		setTimeout(() => {
-			this.setState({
-				movies: [
-					{
-						title: "살인의_추억",
-						poster: "http://cdnweb01.wikitree.co.kr/webdata/editor/201309/06/img_20130906092652_b83d0989.jpg"
-					},
-					{
-						title: "본_얼티메이텀",
-						poster: "http://cdnweb01.wikitree.co.kr/webdata/editor/201309/06/img_20130906092536_e04a74b0.jpg"
-					},
-					{
-						title: "사무라이_픽션",
-						poster: "http://cdnweb01.wikitree.co.kr/webdata/editor/201309/06/img_20130906092600_f0094621.jpg"
-					},
-					{
-						title: "칼",
-						poster: "http://cdnweb01.wikitree.co.kr/webdata/editor/201309/06/img_20130906092837_59dd9cae.jpg"
-					},
-					{
-						title: "셜록",
-						poster: "http://image.auction.co.kr/itemimage/bf/17/85/bf1785425.jpg"
-					}
-				]
-			})
-		}, 5000)
+	_getMovies = async () => {
+		const movies = await this._callApi()
+		this.setState({
+			movies
+		})
 	}
 
 	_renderMovies = () => {
 		const movies = this.state.movies.map((movie, index) => {
-			return <Movie title={movie.title} poster={movie.poster} key={index} />
+			console.log(movie)			
+			return <Movie title={movie.title} poster={movie.medium_cover_image} key={index} />
 		})
 		return movies
+	}
+
+	componentDidMount() {
+		console.log("영화 데이터 요청 하기");		
+		this._getMovies();
 	}
 
 	render() {
